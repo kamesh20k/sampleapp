f340c5c654e6188d2615c0d1fbb58c21f8de3934
kamesh20k <96664751+kamesh20k@users.noreply.github.com>
2022-02-02 15:49:09 +0530

    8th commit

--- sample.py
+++ sample.py
@@ -5,8 +5,8 @@ num2 = 6.4
 num3 = 11
 
 # Add two numbers
-sum = num1 + num2 + num3
+sum = num1 + num2 + num3 
 
 # Display the sum
-print('The sum of {0} and {1} is {2} and {3}'.format(num1, num2,sum))
+print('The sum of {0} and {1} is {2} and {3}'.format(num1, num2, num3,sum))
 
